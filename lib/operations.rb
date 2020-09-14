62aab471757ff001999bcf348d887d50ffaf31f9
Latisha Joe <latisha_joe@yahoo.com>
2020-09-14 09:57:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	speed < 40 || speed > 60
+	if speed < 40 || speed >60
+		return true
+	else speed
+		return false
+ end
 end
-
 
 
 def not_safe?(speed)
- speed <40 || speed > 60 ? true : false
-end
+  speed < 40 || speed > 60 ? true : false 
+end 
